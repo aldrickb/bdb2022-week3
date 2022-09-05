3c2d206e1c4fe5443033e482efaa4cb82ea33782
Aldrick Bonaobra <git@aldrickb.com>
2022-09-05 17:47:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# BDB2022 Week 3 Exercise
+
+For Week 3, you will be developing smart contracts using REMIX IDE and Rinkeby Testing network. Week 3 will consist of four projects you can work on. I recommend you complete at least two projects. Or if you are feeling confident you can complete all the projects. 
+
+## Projects
+
+Week 3 will compose of four projects in total.
+
+1. **Altcoin Project** (Set 1)
+2. **IDO Project** (Set 1)
+3. **NFT Project** (Set 2)
+4. **INO Project** (Set 2)
+
+It is recommended to complete 1 set of project for this week. You can follow the Altcoin track where in you will develop your own ERC-20 token and sell it with your IDO contract. Or you can follow the NFT Project where in you will develop your own ERC-721 token and sell it with your INO contract
+
+
+### Setup
+You are given an initial file that you can initially work on and at the end of the project you will arrive at a working code. After that you need to deploy it to Rinkeby Explorer so that we can check your work.
+
+Clone or download zip this repository to your local device to get started.
+
+The initial file is located inside the contract folder. Then look for the "set-1" folder if you are following the Altcoin track, or look for the "set-2" folder if you are following the NFT track.
+
+You can use [REMIXD](https://remix-ide.readthedocs.io/en/latest/remixd.html), a popular extension of the REMIX IDE where in you can connect directly to your local storage and start coding. Or if you are not confident, you can manually copy paste the smart contract code to REMIX IDE, then paste it back to your local repository whenever you are done coding. This way you can still commit your changes to GitHub and have it checked by the mentors.
+
+## Altcoin Project
+##### /contract/set-1/ERC20.sol
+
+#### Grading Metrics
+The following are the Grading Metrics on how well the student was able to complete the project. Each item correspond to a point/s which perfect points is 10.
+
+1. Fix total supply of the ERC20 contract (2 points)
+2. Deploy ERC20 contract to Rinkeby with
+	a. Your own token name (2 points)
+	b. Your own token symbol (2 points)
+	c. Non-zero total supply (1 point)
+3. A published smart contract code in Rinkeby Explorer (1 point)
+4. Student should be able to send at least 100 of their tokens to the mentor (2 points)
+
+## IDO Project
+##### /contract/set-1/IDO.sol
+
+#### Grading Metrics
+The following are the Grading Metrics on how well the student was able to complete the project. Each item correspond to a point/s which perfect points is 10.
+
+1. Complete the IDO contract from the video (3 points)
+2. Deploy IDO contract to Rinkeby (2 points)
+3. A published smart contract code in Rinkeby Explorer (1 point)
+4. Use your own ERC20 token as sale token (1 points)
+5. Be able to buy tokens, must be in the contract transaction history (1 points)
+6. Be able to withdraw,
+	a. Collected ETH Balance (1 point)
+	b. Remaining sale tokens (1 point)
+
+## NFT Project
+##### /contract/set-1/ERC721.sol
+
+#### Grading Metrics
+The following are the Grading Metrics on how well the student was able to complete the project. Each item correspond to a point/s which perfect points is 10.
+
+1. Complete the MyNFT contract from the video (3 points)
+2. Deploy MyNFT contract to Rinkeby (2 points)
+3. A published smart contract code in Rinkeby Explorer (1 point)
+4. Be able to mint one or many NFT (1 points)
+5. Minted NFT uses the prescribed URI, or your own working URI (1 points) 
+6. Student should be able to send at least one of their NFT to the mentor (2 points)
+
+## INO Project
+##### /contract/set-1/INO.sol
+
+#### Grading Metrics
+The following are the Grading Metrics on how well the student was able to complete the project. Each item correspond to a point/s which perfect points is 10.
+
+1. Complete the INO contract from the video (3 points)
+2. Deploy INO contract to Rinkeby (2 points)
+3. A published smart contract code in Rinkeby Explorer (1 point)
+4. Use your own NFT token as sale token (1 points)
+5. Be able to buy an NFT, must be in the contract transaction history (1 points)
+6. Be able to withdraw,
+	a. Collected ETH Balance (1 point)
+	b. Remaining NFT sale tokens (1 point)
---
+++ contract/set-2/INO.sol
@@ -0,0 +1,213 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ *  @dev Contract module which provides a basic access control mechanism, where
+ *  there is an account (an owner) that can be granted exclusive access to
+ *  specific functions.
+ *
+ *  By default, the owner account will be the one that deploys the contract. This
+ *  can later be changed with {transferOwnership}.
+ *
+ *  This module is used through inheritance. It will make available the modifier
+ *  `onlyOwner`, which can be applied to your functions to restrict their use to
+ *  the owner.
+ */
+
+abstract contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    function _msgSender() internal view virtual returns (address payable) {
+        return payable(msg.sender);
+    }
+
+    /**
+    * @dev Initializes the contract setting the deployer as the initial owner.
+    */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+    * @dev Returns the address of the current owner.
+    */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+    * @dev Leaves the contract without owner. It will not be possible to call
+    * `onlyOwner` functions anymore. Can only be called by the current owner.
+    *
+    * NOTE: Renouncing ownership will leave the contract without an owner,
+    * thereby removing any functionality that is only available to the owner.
+    */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+    * @dev Transfers ownership of the contract to a new account (`newOwner`).
+    * Can only be called by the current owner.
+    */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ *  @dev Interface of the ERC721 standard as defined in the EIP.
+ */
+interface IERC721 {
+    /**
+     * @dev Returns the token collection name.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the token collection symbol.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be have been allowed to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    /**
+     * @dev Transfers `tokenId` token from `from` to `to`.
+     *
+     * WARNING: Usage of this method is discouraged, use {safeTransferFrom} whenever possible.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    /**
+     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
+     * The approval is cleared when the token is transferred.
+     *
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
+     *
+     * Requirements:
+     *
+     * - The caller must own the token or be an approved operator.
+     * - `tokenId` must exist.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address to, uint256 tokenId) external;
+
+    /**
+     * @dev Approve or remove `operator` as an operator for the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
+     *
+     * Requirements:
+     *
+     * - The `operator` cannot be the caller.
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setApprovalForAll(address operator, bool _approved) external;
+
+    /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    /**
+     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
+     *
+     * See {setApprovalForAll}
+     */
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
+
+contract INO is Ownable {
+    
+}
